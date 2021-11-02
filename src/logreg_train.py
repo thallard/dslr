ce9bd61932c159dcc7c9bd245041fdaa3edc1bc7
thallard <thallard@student.42lyon.fr>
2021-11-02 13:46:53 +0100

    Started math part

--- src/logreg_train.py
+++ src/logreg_train.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class LogisticRegression(object):
 
@@ -6,9 +6,23 @@ class LogisticRegression(object):
         self.iterations = iterations
         self.learning_rate = learning_rate
 
+    def model(self, x, theta):
+        return x.dot(theta)
+
     # Sigmoid function
-    def sigmoid(self):
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
+    # Cost function
+    def cost_function(self, x, y, h):
+        m = len(y)
 
+        cost = 1 / m * np.sum(-y * np.log(h)) + (1 - y) * np.log(1 - h)
+        return cost
 
-def main():
+    def gradient_descent(self, X, y, h, theta):
+        gradient_value = np.dot(X.T, (h - y)) / len(y)
+        theta -= self.learning_rate * gradient_value
+        return theta
+
+    def logistic_regression(self, df):
